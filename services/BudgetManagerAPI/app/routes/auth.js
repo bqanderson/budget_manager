ff19043f1dad6032372ab05e15de35219d34aff5
Ivan <bqanderson@me.com>
2018-03-04 20:46:58 -0600

    Create user routes.

--- services/BudgetManagerAPI/app/routes/auth.js
+++ services/BudgetManagerAPI/app/routes/auth.js
@@ -4,8 +4,8 @@ module.exports = (app) => { // Pass our own app to gain ability to set our route
   const api = app.BudgetManagerAPI.app.api.auth;  // Used to gain access to 'BudgetManagerAPI/app/api/auth.js' file
 
   app.route('/')
-    .get((req, res) => res.send('Budget Manager API')); // Send the user 'Budget Manager API' simple text
+    .get((req, res) => res.send('Budget Manager API'));
 
   app.route('/api/v1/auth')
-    .post(api.login(models.User));  // Use our login api method using User model as argument (accessed via POST)
+    .post(api.login(models.User));  // Login api method using User model as argument (accessed via POST)
 }
