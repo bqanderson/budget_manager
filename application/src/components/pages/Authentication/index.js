d79c732d910b7c5fb1089e2383f464f5df6418d9
Brian Q Anderson <bqanderson@me.com>
2018-03-08 00:54:50 -0600

    Complete Authentication component's methods'

--- application/src/components/pages/Authentication/index.js
+++ application/src/components/pages/Authentication/index.js
@@ -6,17 +6,42 @@ const BudgetManagerAPI = `http://${window.location.hostname}:3001`
 export default {
   user: { authenticated: false },
 
-  authenticate(context, credentials, redirect){
+  authenticate(context, credentials, redirect){ // context == Vue component | credentials == username&password | redirect == redirect path
     Axios.post(`${BudgetManagerAPI}/api/v1/auth`, credentials)
-      .then(({ data: { token } }) => {
-        context.$cookie.set('token', token, '1D')
-        context.validLogin = true
-        this.user.authenticated = true
+      .then(({ data: { token } }) => {  // Get token value from data response (the only data needed)
+        context.$cookie.set('token', token, '1D') // Store token value as cookie (set to expire in 1 day)
+        context.validLogin = true // Set components 'validLog' value to true
+        this.user.authenticated = true  // Set user's object 'authenticated' value to true
 
-        if(redirect) router.push(redirect)
-      }).catch(({ response: { data } }) => {
-        context.snackbar = true
-        context.message = data.message
+        if(redirect) router.push(redirect)  // Redirect user to 'redirect' argument
+      }).catch(({ response: { data } }) => {  // Otherwise...
+        context.snackbar = true // ...Set component's 'snackbar' value to true, and...
+        context.message = data.message  //  ...Set component's 'message' to error message
       })
+  },
+
+  signup(context, credentials, redirect){ // context == Vue component | credentials == username&password | redirect == redirect path
+    Axios.post(`${BudgetManagerAPI}/api/v1/signup`, credentials)  // Use Axios to handle HTTP POST request to API
+      .then(({ data: { token } }) => {  // Get token value from data response (the only data needed)
+        context.$cookie.set('token', token, '1D') // Store token value as cookie (set to expire in 1 day)
+        context.validSignup = true  // Set component's 'validSignup' value to true
+        this.user.authenticated = true  // Set user's  object 'authenticated" value to true'
+
+        if(redirect) router.push(redirect)  // Redirect user to 'redirect' argument
+      }).catch(({ response: { data } }) => {  // Otherwise...
+        context.snackbar = true // ...Set component's 'snackbar' value to true, and..
+        context.message = data.message  // ...Set component's 'message' to error message
+      })
+  },
+
+  CheckAuthentication(){  // Check to see if user is authenticated
+    const token = document.cookie
+
+    if(token) this.user.authenticated = true
+    else this.user.authenticated = false
+  },
+
+  GetAuthenticationHeader(context){ // Return Authentication Header
+    return `Bearer ${context.$cookie.get('token')}`
   }
 }
