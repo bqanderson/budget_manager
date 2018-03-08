be83dffc01a1045ee4114ef00253bb4ff71446a0
Brian Q Anderson <bqanderson@me.com>
2018-03-08 15:22:13 -0600

    Start Authentication template (form text fields).

--- application/src/components/pages/Authentication/Authentication.vue
+++ application/src/components/pages/Authentication/Authentication.vue
@@ -1,5 +1,28 @@
 <template>
-  <h1>Auth!</h1>
+  <div class="l-auth-container">
+    <div class="l-auth">
+      <v-form v-model="validLogin">
+        <v-text-field label="Username"
+                      v-model="credentials.username"
+                      prepend-icon="account_box"
+                      :rules="rules"
+                      required
+                      color="light-blue lighten-1">
+        </v-text-field>
+
+        <v-text-field label="Password"
+                      v-model="credentials.password"
+                      prepend-icon="lock"
+                      :rules="rules"
+                      :append-icon="loginPasswordVisible ? 'visibility' : 'visibility_off'"
+                      :append-icon-cb="() => (loginPasswordVisible = !loginPasswordVisible)"
+                      :type="LoginPasswordVisible ? 'text' : 'password'"
+                      color="light-blue lighten-1"
+                      required>
+        </v-text-field>
+      </v-form>
+    </div>
+  </div>
 </template>
 
 <script>
