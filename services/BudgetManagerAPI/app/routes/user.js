dae0bb65f8c5c267ac4bac836d21bed9cef96e42
Brian Q Anderson <bqanderson@me.com>
2018-03-12 11:56:59 -0500

    Clean up user API & Route in services.

--- services/BudgetManagerAPI/app/routes/user.js
+++ services/BudgetManagerAPI/app/routes/user.js
@@ -1,15 +1,7 @@
-const passport = require('passport'),
-      config = require('@config'),
-      models = require('@BudgetManager/app/setup');
+const models = require('@BudgetManager/app/setup');
 
 module.exports = (app) => { // Pass app to gain ability to set routes
   const api = app.BudgetManagerAPI.app.api.user;  // Access 'BudgetManagerAPI/app/api/user.js' file
-
-  app.route('/api/v1/setup')
-    .post(api.setup(models.User))
-
-  app.route('/api/v1/users')
-    .get(passport.authenticate('jwt', config.session), api.index(models.User, app.get('budgetsecret')));
 
   app.route('/api/v1/signup')
     .post(api.signup(models.User));
