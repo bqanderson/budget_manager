572d4be763ae835810b67a33213db9d2d43c10d5
Brian Q Anderson <bqanderson@me.com>
2018-03-12 13:58:27 -0500

    Update User model. Build Budget & Client models.

---
+++ services/BudgetManagerAPI/app/models/budget.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema({
+  client: {
+    type: String,
+    required: true
+  },
+
+  state: {
+    type: String,
+    required: true
+  },
+
+  title: {
+    type: String,
+    require: true
+  },
+
+  total_price: {
+    type: Number,
+    require: true
+  },
+
+  client_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Client'
+  },
+
+  items: [{}]
+});
+
+mongoose.model('Budget', Schema);
--- services/BudgetManagerAPI/app/setup/index.js
+++ services/BudgetManagerAPI/app/setup/index.js
@@ -1,8 +1,13 @@
 // Used to insure that models are loaded before anything else
 const mongoose = require('mongoose'),
       UserModel = require('@BudgetManagerModels/user');
+      BudgetModel = require('@BudgetManagerModels/budget');
+      ClientModel = require('@BudgetManagerModels/client');
 
 const models = {
-  User: mongoose.model('User')
+  User: mongoose.model('User'),
+  Budget: mongoose.model('Budget'),
+  Client: mongoose.model('Client')
 }
+
 module.exports = models;
